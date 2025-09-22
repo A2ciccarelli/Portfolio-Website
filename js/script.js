bc8cf2b128cabad8aa1351f70b6bfe62dae0dfd8
Adrianna Ciccarelli <A2ciccarelli421@gmail.com>
2025-09-22 14:35:36 -0400

    Added media Queries

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".mobile-menu-toggle");
+
+  const mobileMenu = document.querySelector(".mobile-menu-items");
+
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+});
